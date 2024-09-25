208fa859850c1396188ce8a1f2210c5304fc9786
daphnet9 <daphnet9@nycstudents.net>
2024-09-25 15:50:31 +0000

    Test cases

---
+++ Main.java
@@ -0,0 +1,26 @@
+public class Main
+{
+    public static void main(String[] args)
+    {
+        Feeder f = new Feeder();
+        System.out.println(f);
+        //System.out.println(f.currentFood);
+        System.out.println(f.getCurrentFood());
+        Feeder g = new Feeder(500);
+        System.out.println(g.getCurrentFood());
+        System.out.println(Math.random());
+        int random = (int)(Math.random() * 50 + 5);
+        System.out.println(random);
+        System.out.println(Math.random() < 0.05);
+        g.simulateOneDay(12);
+        System.out.println(g.getCurrentFood());
+        Feeder h = new Feeder(1000);
+        h.simulateOneDay(22);
+        System.out.println(h.getCurrentFood());
+        Feeder i = new Feeder(100);
+        i.simulateOneDay(5);
+        System.out.println(i);
+        Feeder j = new Feeder(2400);
+        System.out.println(j.simulateManyDays(5, 10));
+    }
+}
